bf53d531a6dcf30fc6acd4d9d242e21f4de0622f
Eli Winslow <eliwinslow@Elis-MacBook-Air.local>
2023-01-21 11:18:47 -0800

    Data get and post working

--- bikeskagitreact/src/components/NewEntry.jsx
+++ bikeskagitreact/src/components/NewEntry.jsx
@@ -1,55 +1,68 @@
-import React from "react";
+import { useState } from "react";
 
-function NewEntry() {
+const NewEntry = () => {
+    const [date, setDate] = useState('')
+    const [startloc, setStartLoc] = useState('')
+    const [endloc, setEndLoc] = useState('')
+    const [mileage, setMileage] = useState('')
+    const [elevation, setElevation] = useState('')
+    const [notes, setNotes] = useState('')
+    const [restpin, setRestPin] = useState('')
+    const [error, setError] = useState(null)
+    
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const ride = {date, startloc, endloc, mileage, elevation, notes, restpin}
+
+        const response = await fetch('/api/rides', {
+            method: 'POST',
+            body: JSON.stringify(ride),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const json = await response.json()
+        if(!response.ok) {
+            setError(json.error)
+        }
+        if (response.ok) {
+            setError(null)
+            console.log('new ride added', json)
+        }
+    }
+
+
     return (
-    <div>
-        <p> New Entry component</p>
-            <form class="w-full max-w-sm">
-                <div class="md:flex md:items-center mb-6">
-                    <div class="md:w-1/3">
-                        <label>
-                            Date:
-                            <input type="text" name="date" />
-                        </label>
-                    </div>    
-                </div>
-            </form>
-{/* 
-                <label>
-                    Starting Location:
-                    <input type="text" name="startloc" />
-                </label>
-                
-                <label>
-                    End Location:
-                    <input type="text" name="endloc" />
-                </label>
+    <form className= "create" onSubmit={handleSubmit}>
+        <h2> Add a new ride to the collection!</h2>
+        <label>Date: </label>
+        <input type="text" onChange={(e) => setDate(e.target.value)} value={date}/>
 
-                <label>
-                    Mileage:
-                    <input type="text" name="mileage" />
-                </label>
-                
-                <label>
-                    Elevation:
-                    <input type="text" name="elevation" />
-                </label>
-                
-                <label>
-                    Notes:
-                    <input type="text" name="notes" />
-                </label>
-                
-                <label>
-                    Restspot Pin:
-                    <input type="text" name="restpin" />
-                </label>
-                
-                
-                <input type="submit" value="Submit" />
-            </form> */}
-    </div>
-    );
+        <label>Start Location:  </label>
+        <input type="text" onChange={(e) => setStartLoc(e.target.value)} value={startloc}/>
+
+        <label>End Location: </label>
+        <input type="text" onChange={(e) => setEndLoc(e.target.value)} value={endloc}/>
+
+        <label>Miles: </label>
+        <input type="text" onChange={(e) => setMileage(e.target.value)} value={mileage}/>
+
+        <label>Elevation:  </label>
+        <input type="text" onChange={(e) => setElevation(e.target.value)} value={elevation}/>
+
+        <label>Notes: </label>
+        <input type="text" onChange={(e) => setNotes(e.target.value)} value={notes}/>
+
+        <label>Rest Spot:  </label>
+        <input type="text" onChange={(e) => setRestPin(e.target.value)} value={restpin}/>
+    
+        <button>Submit Ride</button>
+        {error && <div className="error">{error}</div>}
+
+    </form>
+)
 }
 
 export default NewEntry;
